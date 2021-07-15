aa3ce2c927ffb055f4941fca13c4b92d94d2cafe
Cindano Jonathan <65620947+Snooker4Real@users.noreply.github.com>
2021-07-15 14:01:03 +0200

    Initial commit

---
+++ src/com/hb/panierpiquenique/PiqueNiqueMain.java
@@ -0,0 +1,25 @@
+package com.hb.panierpiquenique;
+
+public class PiqueNiqueMain {
+
+    public static void afficherMenu(){
+
+        System.out.println("***** Panier de Pique-Nique *****");
+        System.out.println("");
+        System.out.println("Tous les produits qui compose le panier sont issues de l'agriculture biologique");
+
+        System.out.println("Combient de personnes pour votre panier ?");
+
+        System.out.println("(1) 2 personnes");
+        System.out.println("(2) 4 personnes");
+        System.out.println("(3) 6 personnes");
+
+
+
+
+    }
+    public static void main(String[] args) {
+        afficherMenu();
+
+    }
+}
